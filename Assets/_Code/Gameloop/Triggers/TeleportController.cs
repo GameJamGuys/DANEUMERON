f0688972815d1a776f01b415d1304da92d5b6454
LocalPhilosopher <rekunigato@gmail.com>
2023-10-20 17:43:58 +0300

    - add trigger

---
+++ Assets/_Code/Gameloop/Triggers/TeleportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TarodevController;
+using UnityEngine;
+
+namespace _Code.Gameloop.Triggers
+{
+    public class TeleportController : MonoBehaviour
+    {
+        [SerializeField] private PlayerController player;
+        [SerializeField] private Transform posA;
+        [SerializeField] private Transform posB;
+
+        private void Update()
+        {
+            if (player.transform.position.x < posA.position.x)
+                player.transform.position = new Vector3(posB.position.x,player.transform.position.y,
+                    player.transform.position.z);
+            
+            if (player.transform.position.x > posB.position.x)
+                player.transform.position = new Vector3(posA.position.x,player.transform.position.y,
+                    player.transform.position.z);
+        }
+    }
+}
