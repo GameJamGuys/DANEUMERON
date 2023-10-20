b776f96e2c27fc018c1b0b87603fa1d09d399546
Aristarkh Abramovsky <ari.bram@ya.ru>
2023-10-20 18:56:50 +0300

    TestLevel upd

---
+++ Assets/_Code/Player/DeadComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using TarodevController;
+
+namespace _Code
+{
+    public class DeadComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerAnimator _anim;
+
+        public void Dead()
+        {
+            _anim.OnDead();
+        }
+
+
+    }
+}
