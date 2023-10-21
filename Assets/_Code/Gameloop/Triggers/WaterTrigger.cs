fe01fca3250ca6849a94224608d556bef94fef77
LocalPhilosopher <rekunigato@gmail.com>
2023-10-21 20:48:29 +0300

    - upd

---
+++ Assets/_Code/Gameloop/Triggers/WaterTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Audio;
+using UnityEngine;
+
+namespace _Code.Gameloop.Triggers
+{
+    public class WaterTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.GetComponent<Rigidbody2D>())
+                AudioBox.Instance.Play("Water");
+        }
+    }
+}
