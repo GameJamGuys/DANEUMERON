955eee598729a8f35d6ed736ca5ddf9e4bc06e1d
LocalPhilosopher <rekunigato@gmail.com>
2023-10-21 13:05:31 +0300

    - upd glass

--- Assets/_Code/Gameloop/Triggers/BreakableObject.cs
+++ Assets/_Code/Gameloop/Triggers/BreakableObject.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 
 namespace _Code.Gameloop.Triggers
 {
     public class BreakableObject : MonoBehaviour
     {
+        [SerializeField] private Transform glare;
+        [SerializeField] private Transform end;
+        [SerializeField] private float duration;
+
+        private void Awake()
+        {
+            glare.DOMove(end.position, duration).SetLoops(-1);
+        }
+
         public void Broke()
         {
             Destroy(gameObject);
