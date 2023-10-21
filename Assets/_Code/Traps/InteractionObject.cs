1f31230ee950df191339bcde9485ee824df93259
EvaAvocado <2002mskirill2002@gmail.com>
2023-10-21 16:22:27 +0300

    Add Button Two System

--- Assets/_Code/Traps/InteractionObject.cs
+++ Assets/_Code/Traps/InteractionObject.cs
@@ -6,10 +6,29 @@ namespace _Code.Traps
     public class InteractionObject : MonoBehaviour
     {
         [SerializeField] private UnityEvent interactAction;
+        [SerializeField] private UnityEvent secondInteractionAction;
+        [SerializeField] private UnityEvent thirdInteractionAction;
         
+
+        public UnityEvent SecondInteractionAction
+        {
+            get => secondInteractionAction;
+            set => secondInteractionAction = value;
+        }
+
         public void Interact()
         {
             interactAction?.Invoke();
+        }
+
+        public void SecondInteract()
+        {
+            secondInteractionAction?.Invoke();
+        }
+
+        public void ThirdInteract()
+        {
+            thirdInteractionAction?.Invoke();
         }
     }
 }
